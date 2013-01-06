9897b010e7398a35ed502299a1821e36aec49c09
oxman <informatique@bouh.org>
2013-01-06 12:00:48 +0100

    enable logging (remove slf4j-nop) new dependency pbkdf2 to crypt password

--- project/Build.scala
+++ project/Build.scala
@@ -9,13 +9,14 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
         "com.typesafe" % "slick_2.10.0-RC1" % "0.11.2",
-        "org.slf4j" % "slf4j-nop" % "1.6.4",
         "mysql" % "mysql-connector-java" % "5.1.20",
         jdbc
     )
 
+    val pbkdf2Project = RootProject(uri("git://github.com/oxman/pbkdf2-scala.git"))
+
     val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here
-    )
+    ).dependsOn(pbkdf2Project)
 
 }
