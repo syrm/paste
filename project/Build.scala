32cb8040cbe5f7f09174b1c2d367cdac8edf8ce2
oxman <informatique@bouh.org>
2013-01-17 11:25:39 +0100

    Fix style

--- project/Build.scala
+++ project/Build.scala
@@ -4,19 +4,17 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "Paste"
-    val appVersion      = "1.2.2"
+  val appName = "Paste"
+  val appVersion = "1.2.2"
 
-    val appDependencies = Seq(
-        "org.squeryl" %% "squeryl" % "0.9.5-6",
-        "mysql" % "mysql-connector-java" % "5.1.20",
-        jdbc
-    )
+  val appDependencies = Seq(
+    "org.squeryl" %% "squeryl" % "0.9.5-6",
+    "mysql" % "mysql-connector-java" % "5.1.20",
+    jdbc)
 
-    val pbkdf2Project = RootProject(uri("git://github.com/oxman/pbkdf2-scala.git"))
+  val pbkdf2Project = RootProject(uri("git://github.com/oxman/pbkdf2-scala.git"))
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-      // Add your own project settings here
-    ).dependsOn(pbkdf2Project)
+  val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here
+  ).dependsOn(pbkdf2Project)
 
 }
